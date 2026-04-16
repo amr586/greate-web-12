8c038acec516cbb7ee05bfbe7d84c3896a3dae6d
0x1ris <mahmoud152g2@gmail.com>
2026-04-16 14:27:08 +0000

    chore: remove duplicate backend0 folder

--- backend0/server.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-require('dotenv').config({ path: path.join(__dirname, '.env') });
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
